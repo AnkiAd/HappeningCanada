3811f275dc6e5e407d4c764c600d3a60f53f197e
Ani Adhikary <aniadhikary@Anis-MacBook-Air.local>
2018-03-10 00:06:35 +0530

    First commit

---
+++ HappeningCanada/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappeningCanada
+//
+//  Created by Ani Adhikary on 10/03/18.
+//  Copyright © 2018 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
