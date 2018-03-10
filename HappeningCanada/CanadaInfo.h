05f178e0bba091366b467fd6c7fb2c164b57e169
Ani Adhikary <aniadhikary@anis-macbook-air.local>
2018-03-11 01:07:00 +0530

    Added Model file CanadaInfo

---
+++ HappeningCanada/CanadaInfo.h
@@ -0,0 +1,15 @@
+//
+//  CanadaInfo.h
+//  HappeningCanada
+//
+//  Created by Ani Adhikary on 11/03/18.
+//  Copyright © 2018 Example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CanadaInfo : NSObject
+    @property NSString *titleDetail;
+    @property NSString *descriptionDetail;
+    @property NSString *imageHref;
+@end
---
+++ HappeningCanada/CanadaInfo.m
@@ -0,0 +1,15 @@
+//
+//  CanadaInfo.m
+//  HappeningCanada
+//
+//  Created by Ani Adhikary on 11/03/18.
+//  Copyright © 2018 Example. All rights reserved.
+//
+
+#import "CanadaInfo.h"
+
+@implementation CanadaInfo
+    @synthesize titleDetail;
+    @synthesize descriptionDetail;
+    @synthesize imageHref;
+@end
