305110e8cb7a5776c02fe3f4a93187d8adc21f1e
Ani Adhikary <aniadhikary@anis-macbook-air.local>
2018-03-20 22:36:43 +0530

    Indentation done

--- HappeningCanada/Model/CanadaInfoRow.h
+++ HappeningCanada/Model/CanadaInfoRow.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface CanadaInfoRow : NSObject
+
 @property (strong, nonatomic) NSString *titleDetail;
 @property (strong, nonatomic) NSString *descriptionDetail;
 @property (strong, nonatomic) NSString *imageHref;
